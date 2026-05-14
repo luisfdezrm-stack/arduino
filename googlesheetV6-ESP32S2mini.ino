622e9051e13305bd743652cd93a1f01d95512ffe
luisfdezrm-stack <luisfdezrm@gmail.com>
2026-05-14 23:34:46 +0200

    Update version to 6.1 and improve WiFi handling

--- googlesheetV6-ESP32S2mini.ino
+++ googlesheetV6-ESP32S2mini.ino
@@ -1,22 +1,25 @@
 /*
-Cambios en la versión 6 VERSIÓM PARA ESP32 S2 mini
+Cambios en la versión 6.0 VERSIÓM PARA ESP32 S2 mini
  se incluye:
 analiza si cada dato es distinto del anterior, y sólo envía el dato si el cambio es significativo.	
-
+descarga desde GitHub
 */
 
 
 #include <WiFi.h>
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
+#include <WiFiMulti.h>
+#include <WiFiManager.h>
+#include <HTTPUpdate.h>
 #include <DHT.h>
 #include <Wire.h>
 #include <Adafruit_ADS1X15.h>
 #include <ArduinoOTA.h>
-#include <WiFiManager.h>
 #include <LittleFS.h>  //para almacenar vbles en ESP32 memoria no volátil
 
 Adafruit_ADS1115 ads; // Creamos la instancia del objeto ADS1115
+WiFiMulti wifiMulti;
 
 #define DHTPIN 5 // en el ESP32 es 4
 #define DHTTYPE DHT11
@@ -24,14 +27,17 @@ Adafruit_ADS1115 ads; // Creamos la instancia del objeto ADS1115
 #define SCL_PIN 9 // para el ESP32 S2 mini
 DHT dht(DHTPIN, DHTTYPE);
 
+const float VERSION_ACTUAL = 6.1;
+
 //const float NIVEL_MAX = 15.0;    // Nivel máximo del sensor en metros
 //const float I_MIN = 4.0;         // Corriente mínima sensor (0 metros)
 //const float I_MAX = 20.0;        // Corriente máxima sensor (15 metros)
 //const float V_MAX = 10.0;// tensión máxima del regulador 4-20 a 0-10V (jumper 2 quitado. Con los dos jumper puestos, sería 0-3.3V)
 const int ledPin = 14; // en el ESP32, era el 2
-const char* DEFAULT_SSID = "ctlnsrz16vdfn";
-const char* DEFAULT_PASS = "ctlnsrz16vdfne";
-char scriptURL[150] = "https://script.google.com/macros/s/AKfycbybdpPHvBwuFokiMDu765gOQx3REcbAIuOuv6_IyFGhVeGuOyxrXeCctPgZBUXIfOduEA/exec";
+//const char* DEFAULT_SSID = "ctlnsrz16vdfn";
+//const char* DEFAULT_PASS = "ctlnsrz16vdfne";
+//char scriptURL[150] = "https://script.google.com/macros/s/AKfycbybdpPHvBwuFokiMDu765gOQx3REcbAIuOuv6_IyFGhVeGuOyxrXeCctPgZBUXIfOduEA/exec";
+char scriptURL[150] = "";
 bool shouldSaveConfig = false;
 bool adsOK = false;
 float voltaje, nivel, temp, hum;
@@ -49,18 +55,24 @@ const float UMBRAL_TEMP = 0.5;    // Cambio mínimo de 0.5 grados
 const float UMBRAL_HUM = 1.0;     // Cambio mínimo de 1% 
 
 
+const char* URL_VERSION = "https://raw.githubusercontent.com/luisfdezrm-stack/arduino/main/version_actual";
+const char* URL_BINARIO = "https://raw.githubusercontent.com/luisfdezrm-stack/arduino/main/googlesheetV6-ESP32S2mini.ino.bin";
+
+
 // ==========================================
 //                SET-UP
 // ==========================================
 void setup() {
   configurarSerial();
   configurarSistemaArchivos();
-  gestionarConexionWifi();
+  configurarWiFiDesdeFS();
+  if (wifiMulti.run() != WL_CONNECTED) {gestionarConexionWifi(); }
   configurarOTA();
   inicializarHardware();
-  LittleFS.remove("/config_url.txt"); //limpia la URL que está guardada en la memoria
+//  LittleFS.remove("/config_url.txt"); //limpia la URL que está guardada en la memoria
+  checkParaActualizar();
   Serial.println("\n>>> Sistema Inicializado Correctamente (SETUP)");
-}
+ }
 
 
 
@@ -75,7 +87,6 @@ void loop() {
     ejecutarCicloLectura();
  
 
-
 if (haCambiadoElDato()) {
       gestionarEnvioDatos();
       // Guardamos el estado actual como último enviado
@@ -85,6 +96,7 @@ if (haCambiadoElDato()) {
     } else {
       Serial.println(">>> Datos estables. No se requiere envío.");
     }
+    checkParaActualizar();
   }
 }
 
@@ -93,19 +105,53 @@ if (haCambiadoElDato()) {
 //               FUNCIONES
 // ==========================================
 
-
 bool haCambiadoElDato() {
   bool cambioV = abs(voltaje - last_voltaje) >= UMBRAL_VOLTAJE;
   bool cambioT = abs(temp - last_temp) >= UMBRAL_TEMP;
   bool cambioH = abs(hum - last_hum) >= UMBRAL_HUM;
-  return (cambioV || cambioT || cambioH);
-}
-
+  return (cambioV || cambioT || cambioH);}
 
 void saveConfigCallback() {shouldSaveConfig = true;} // Callback de WiFiManager para avisar que debe guardar datos
 
-
 void configurarSerial() {Serial.begin(115200); delay(100); }
+
+void checkParaActualizar() {
+  HTTPClient http;
+  WiFiClientSecure client;
+  client.setInsecure();
+  http.setUserAgent("ESP32-S2-Mini");
+  Serial.println("Comprobando actualizaciones en GitHub...");
+  http.begin(client, URL_VERSION); 
+  int httpCode = http.GET();
+
+  if (httpCode == HTTP_CODE_OK) {
+    String payload = http.getString();
+    float version_remota = payload.toFloat();
+
+    // 2. Comparar: ¿Es la versión de la web mayor que la mía?
+    if (version_remota > VERSION_ACTUAL) {
+      Serial.printf("Nueva versión detectada: %.1f. Actualizando...\n", version_remota);
+
+     // Configuramos el timeout para descargas pesadas
+      httpUpdate.setLedPin(ledPin, LOW); // Opcional: parpadea el LED durante la descarga
+        // 3. Ejecutar la descarga del binario solo si la versión es superior
+      t_httpUpdate_return ret = httpUpdate.update(client, URL_BINARIO);
+    switch (ret) {
+    case HTTP_UPDATE_FAILED: Serial.printf("Error: %s\n", httpUpdate.getLastErrorString().c_str()); break;
+    case HTTP_UPDATE_NO_UPDATES: Serial.println("No hay actualizaciones."); break;
+    case HTTP_UPDATE_OK: Serial.println("Actualización terminada! Nueva versión zzzzzzzzzzzzzzzzzzzzzzzz descargado desde github zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"); break;
+  }
+      
+      // (Manejo de errores del update...)
+    } else {
+      Serial.println("El firmware está al día.");
+    }
+  }
+  else {
+    Serial.printf("Error al conectar con GitHub para verificar versión. Código HTTP: %d\n", httpCode);
+  }
+  http.end();  
+}
 
 
 void configurarSistemaArchivos() {
@@ -117,7 +163,6 @@ void configurarSistemaArchivos() {
     }
     Serial.println("LittleFS formateado correctamente.");
   }
-
   if (LittleFS.exists("/config_url.txt")) {    // Leer archivo si existe
     File f = LittleFS.open("/config_url.txt", "r");
     if (f) {
@@ -146,27 +191,23 @@ void gestionarConexionWifi() {
   wm.setConfigPortalTimeout(180); // 3 minutos intentando portal (opcional)
   // Intentar conectar (Si falla crea AP "ESP32_Sensor_Config")
   if (!wm.autoConnect("ESP32_Sensor_Config")) {
-    Serial.println("No hay credenciales guardadas o fallo. Probando WiFi por defecto......");
-    WiFi.begin(DEFAULT_SSID, DEFAULT_PASS);
+    Serial.println("No hay credenciales guardadas o fallo. Abrimos portal como Punto de acceso, AP: ESP32_Sensor_Config......");
+   // WiFi.begin(DEFAULT_SSID, DEFAULT_PASS);
      int retries = 0;
     while (WiFi.status() != WL_CONNECTED && retries < 20) {
       delay(500);
       Serial.print(".");
       retries++;
     }
-
  if (WiFi.status() != WL_CONNECTED) {
       Serial.println("\nFallback fallido. Abriendo portal...");
       wm.startConfigPortal("ESP32_Sensor_Config");
     }
-
   }
-  // Guardar la URL si se cambió en el portal
-  if (shouldSaveConfig) {
+  if (shouldSaveConfig) {    // Guardar la URL si se cambió en el portal
  //   strcpy(scriptURL, custom_script_url.getValue()); Sustituyo esta propuesta por Gemini, por la siguiente de ChatGPT
 strncpy(scriptURL, custom_script_url.getValue(), sizeof(scriptURL) - 1);
 scriptURL[sizeof(scriptURL) - 1] = '\0';
-
     File f = LittleFS.open("/config_url.txt", "w");
     if (f) {
       f.print(scriptURL);
@@ -179,13 +220,31 @@ scriptURL[sizeof(scriptURL) - 1] = '\0';
 }
 
 
-void configurarOTA() {
- ArduinoOTA.setHostname("esp32-sensor-nivel");
- // Aquí se podría añadir ArduinoOTA.setPassword("tu_clave");
- ArduinoOTA.begin();
+void configurarWiFiDesdeFS() {
+  if (LittleFS.exists("/wifi_creds.txt")) {
+    File f = LittleFS.open("/wifi_creds.txt", "r");
+    while (f.available()) {
+      String line = f.readStringUntil('\n');
+      line.trim();
+      int sep = line.indexOf(';');
+      if (sep != -1) {
+        String ssid = line.substring(0, sep);
+        String pass = line.substring(sep + 1);
+        wifiMulti.addAP(ssid.c_str(), pass.c_str());
+        Serial.printf("Añadida red: %s\n", ssid.c_str());
+      }
+    }
+    f.close();
+  }
+
+  Serial.println("Conectando WiFi...");
+  if (wifiMulti.run() == WL_CONNECTED) {
+    Serial.print("Conectado a: "); Serial.println(WiFi.SSID());
+  }
 }
 
-
+void configurarOTA() { ArduinoOTA.setHostname("esp32-sensor-nivel"); ArduinoOTA.begin();}
+ 
 void inicializarHardware() {
   Wire.begin(SDA_PIN, SCL_PIN); // especifico para S2 mini
   pinMode(ledPin, OUTPUT);
@@ -194,7 +253,6 @@ void inicializarHardware() {
   adsOK = ads.begin();
   if (!adsOK) {
   Serial.println("ADS1115 no detectado. Continuando sin él.");
-//    ESP.restart();  //eliminado para evitar bloqueos, y permitir actualización OTA
   }
 // Ajuste de ganancia para señales de hasta 4.096V (para medir hasta 6.144V, se usaría GAIN_TWOTHIRDS
   ads.setGain(GAIN_ONE); 
@@ -203,20 +261,14 @@ void inicializarHardware() {
 }
 
 
-void ejecutarCicloLectura() {
-  blinkLED();
-  leerADS1115();
-  leerDHT11();
-}
-
+void ejecutarCicloLectura() { blinkLED(); leerADS1115(); leerDHT11(); }
 
 void leerADS1115() {
  if (!adsOK) return;
  long suma = 0;
  for(int i = 0; i < 50; i++) {
  suma += ads.readADC_SingleEnded(0);
- delay(100); //evita leer constantemente el mismo valor
- }
+ delay(100); }
  int sensorValue = suma / 50;
  float voltaje0 = sensorValue * 0.1875 / 1000.0;
  voltaje = voltaje0;
@@ -224,19 +276,13 @@ void leerADS1115() {
 // Factor ajustado manualmente tras calibración real (no coincide con GAIN_ONE teórico)
 // voltaje0 = adc0 * 0.1875 / 1000.0;
 // nivel = NIVEL_MAX * voltaje / V_MAX;  // 1V = 1.5m, 3V = 4.5m
-   
   nivel = (1.524 * voltaje) - 0.283;
   Serial.printf("Lectura ADS: %d | Voltaje: %.2fV | Nivel: %.2f m\n", sensorValue, voltaje, nivel);
 }
 
 void gestionarEnvioDatos() {
-  if (WiFi.status() == WL_CONNECTED) {
-    enviarAGoogleSheets();
-  } else {
-    Serial.println("WiFi desconectado. Reintentando...");
-     WiFi.reconnect();  // anteriormente WiFi.begin(); 
-  }
-}
+  if (WiFi.status() == WL_CONNECTED) { enviarAGoogleSheets(); } 
+    else { Serial.println("WiFi desconectado. Reintentando..."); WiFi.reconnect(); } }
 
 void enviarAGoogleSheets() {
   HTTPClient http;
@@ -258,7 +304,6 @@ void enviarAGoogleSheets() {
   http.end();
 }
 
-
 void leerDHT11() {
   float t = dht.readTemperature();
   float h = dht.readHumidity();
@@ -271,10 +316,4 @@ void leerDHT11() {
   Serial.printf("Temp: %.1f C | Humedad: %.1f %%\n", temp, hum);
 }
 
-
-void blinkLED() {
-  digitalWrite(ledPin, HIGH);
-  delay(100);
-  digitalWrite(ledPin, LOW);
-}
-
+void blinkLED() {digitalWrite(ledPin, HIGH); delay(100); digitalWrite(ledPin, LOW); }
